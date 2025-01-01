b945221db6d075e28c985a4e064a789e9bb474ad
mdashlw <mdashlw@gmail.com>
2025-01-01 12:47:52 +0800

    add /media endpoints

--- src/index.ts
+++ src/index.ts
@@ -11,4 +11,22 @@ router.get("/reblogs/:postId", async (request, env) => {
   return await sql`SELECT * FROM reblogs WHERE root_post_id = ${request.params.postId} ORDER BY reblog_post_id DESC`;
 });
 
+router.get("/media/key/:key", async (request, env) => {
+  const sql = postgres(env.DB_URL);
+
+  return await sql`SELECT * FROM media WHERE key = ${request.params.key}`;
+});
+
+router.get("/media/key_a/:key_a", async (request, env) => {
+  const sql = postgres(env.DB_URL);
+
+  return await sql`SELECT * FROM media WHERE key_a = ${request.params.key_a}`;
+});
+
+router.get("/media/key_b/:key_b", async (request, env) => {
+  const sql = postgres(env.DB_URL);
+
+  return await sql`SELECT * FROM media WHERE key_b = ${request.params.key_b}`;
+});
+
 export default { ...router };
